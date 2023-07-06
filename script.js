ec0b05dd5b689de8ef7e924288d745fbb0b8e9cb
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2023-07-06 05:06:47 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Email.send({
+//     SecureToken : "e2478d91-a1ce-4463-824f-a7e2ee36560d",
+//     To : 'sameerkhannn9412@gmail.com',
+//     From : "sameerkhannn9412@gmail.com",
+//     Subject : "This is the subject",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
+alert("hi sameer")
+const Name=document.getElementById("Name");
+const email=document.getElementById("email");
+const subject=document.getElementById("subject");
+const message=document.getElementById("message");
+submit.addeventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("clicked");
+}) 
